934d961165b21c4498f2a6c3db1a156e997ff17a
ApplePieGiraffe <itsmejoshwoodson@gmail.com>
2021-02-27 08:21:40 +0300

    Configure additional setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,44 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// imports
+
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+
+// global styles
+
+const GlobalStyles = createGlobalStyle`
+	*,
+	*::before,
+	*::after {
+		box-sizing: border-box;
+		margin: 0;
+		padding: 0;
+	}
+`
+
+// themes
+
+const lightTheme = {
+	colors: {
+		primary: '#f8f8fb'
+	}
 }
 
-export default MyApp
+const darkTheme = {
+	colors: {
+		primary: '#141625'
+	}
+}
+
+// component
+
+function App({ Component, pageProps }) {
+	return (
+		<>
+			<GlobalStyles/>
+			<ThemeProvider theme={lightTheme}>
+				<Component {...pageProps}/>
+			</ThemeProvider>
+		</>
+	)
+}
+
+export default App
