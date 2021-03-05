f32aa05e8ad24e81d5f4ab31d853820239f59129
ApplePieGiraffe <itsmejoshwoodson@gmail.com>
2021-03-05 23:35:39 +0300

    Move themes/components from _app.js to Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,10 @@
+import { useState, useEffect } from 'react'
+import Store from 'store'
 import styled, { ThemeProvider } from 'styled-components'
-import GlobalStyles from './GlobalStyles'
 import { light, dark } from '../data/Themes'
+import GlobalStyles from './GlobalStyles'
+import Main from '../components/Main'
+import Sidebar from '../components/Sidebar'
 
 const Wrapper = styled.div`
 	display: flex;
@@ -11,12 +15,27 @@ const Wrapper = styled.div`
 	}
 `
 
-export default function Layout({ theme, children }) {
+export default function Layout({ children }) {
+    const [theme, setTheme] = useState('light')
+
+	useEffect(() => {
+		if (Store.get('theme') === undefined) {
+			Store.set('theme', 'light')
+		}
+		setTheme(Store.get('theme'))
+	}, [setTheme])
+
+    function toggleTheme() {
+		Store.set('theme', theme === 'light' ? 'dark' : 'light')
+        setTheme(Store.get('theme'))
+    }
+
     return (
         <ThemeProvider theme={theme === 'light' ? light : dark}>
             <GlobalStyles/>
             <Wrapper>
-                {children}
+                <Sidebar toggleTheme={toggleTheme}/>
+                <Main>{children}</Main>
             </Wrapper>
         </ThemeProvider>
     )
--- pages/_app.js
+++ pages/_app.js
@@ -4,8 +4,6 @@ import Store from 'store'
 import data from '../data/data.json'
 
 import Layout from '../components/Layout'
-import Main from '../components/Main'
-import Sidebar from '../components/Sidebar'
 
 export default function App({ Component, pageProps }) {
 	const [invoices, setInvoices] = useState(null)
@@ -17,27 +15,9 @@ export default function App({ Component, pageProps }) {
 		setInvoices(Store.get('invoices'))
 	}, [setInvoices])
 
-	const [theme, setTheme] = useState('light')
-
-	useEffect(() => {
-		if (Store.get('theme') === undefined) {
-			Store.set('theme', 'light')
-		}
-		setTheme(Store.get('theme'))
-	}, [setTheme])
-
-    function toggleTheme() {
-		Store.set('theme', theme === 'light' ? 'dark' : 'light')
-        setTheme(Store.get('theme'))
-    }
-
 	return (
-		<Layout theme={theme}>
-			<Sidebar toggleTheme={toggleTheme}/>
-			<Main>
-				<Component {...pageProps}/>
-				{invoices ? <span>{invoices[0].id}</span> : <span>Loading...</span>}
-			</Main>
+		<Layout>
+			<Component {...pageProps}/>
 		</Layout>
 	)
 }
